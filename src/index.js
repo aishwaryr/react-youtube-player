6881554b9a0d16d90ed00b3331724c9c392278a7
aishwaryr <araj713@gmail.com>
2017-08-03 00:47:41 +0530

    Added state to searchBar comp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyD4lLVfRzb53Z0dLkfTfw6IrbgwBDOGrRE';
+
 // Create a new component, This component should produce some HTML
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 // Take this comp's generated HTML and put it on the Page (DOM)
 ReactDOM.render(<App />, document.querySelector('#main'));
