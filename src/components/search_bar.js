b50a1adce1090fbc0693f149181f3b924ac5ab9c
aishwaryr <araj713@gmail.com>
2017-08-04 00:28:57 +0530

    Video-list, details components in progress

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = { term: 'sdsd' };
+        this.state = { term: '' };
     }
 
     render() {
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyD4lLVfRzb53Z0dLkfTfw6IrbgwBDOGrRE';
 
+
 // Create a new component, This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'Ronaldo'}, (videos) => {
+            console.log(videos);
+            this.setState({ videos });
+            // this.setState({ videos: videos }); when key and value are same
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 // Take this comp's generated HTML and put it on the Page (DOM)
 ReactDOM.render(<App />, document.querySelector('#main'));
